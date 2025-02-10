9d3f4632dd147bd7481de4cf0226ff4831d6ad5c
Alan Albes <aeva030224@itsuruapan.edu.mx>
2025-02-09 23:14:18 -0600

    Se actulizo la pestaña fallas, base de datos, etc.

--- PaginaWeb/server/serverBaseDatos.py
+++ PaginaWeb/server/serverBaseDatos.py
@@ -37,5 +37,26 @@ def agregar_reporte():
     db.session.commit()
     return jsonify({"mensaje": "Reporte agregado correctamente"}), 201
 
+# Ruta para actualizar el estado
+@app.route('/reportes/<int:id>', methods=['PUT'])
+def actualizar_reporte(id):
+    data = request.json
+    nuevo_estado = data.get("estado")
+
+    if not nuevo_estado:
+        return jsonify({"error": "Estado requerido"}), 400
+
+    reporte = Reportes.query.get(id)
+
+    if reporte is None:
+        return jsonify({"error": "Reporte no encontrado"}), 404
+
+    reporte.estado = nuevo_estado
+    db.session.commit()
+
+    return jsonify({"message": "Estado actualizado correctamente", "nuevo_estado": nuevo_estado}), 200
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
