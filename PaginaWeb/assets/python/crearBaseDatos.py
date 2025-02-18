2577df1e24021082c9e2bf0abae9c4c13a1e7a0b
Alan Albes <alan.alveas.e@gmail.com>
2025-02-18 09:04:46 -0600

    Login hecho

--- PaginaWeb/assets/python/crearBaseDatos.py
+++ PaginaWeb/assets/python/crearBaseDatos.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, app
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -29,9 +29,27 @@ class Inventario(db.Model):
 
     def __repr__(self):
         return f'Reporte({self.id}, {self.nombre}, {self.modelo}, {self.noSerie}, {self.ubicacion}, {self.estado})'
+    
+# Definir el modelo de la tabla login
+class Login(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    usuario = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    def __repr__(self):
+        return f'Reporte({self.id}, {self.nombre}, {self.usuario}, {self.password})'
+    
+    # Insertamos el usuario para el login
+def insert_data():
+    with app.app_context():
+        nuevo_usuario = Login(nombre="Administrador", usuario="admin", password="admin")
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+        print("Usuario insertado correctamente")
 
 # Crear la base de datos y las tablas
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        insert_data()
         print("Base de datos creada exitosamente.")
