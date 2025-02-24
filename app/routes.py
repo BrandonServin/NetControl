d403b3135f9ec2e2af2ab0e3500ff8e4b3fbe4a7
Luis Hernandez <hegl030420@itsuruapan.edu.mx>
2025-02-24 09:22:50 -0600

    detalles de la red

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ from app.models.nmap import scan_network
 from app.models.login import Login
 from app.models.inventario import Inventario, UnidadActiva
 from app.models.reportes import Reportes
+from app.NomRed import obtener_info_red
 
 main = Blueprint("main", __name__)
 
@@ -241,6 +242,14 @@ def actualizar_reporte(id):
         ),
         200,
     )
+    
+# ruta para obtener nombre y contraseña de la red
+
+#Metodo para obtener detalles de la red
+@main.route('/get_wifi_info', methods=['GET'])
+def get_wifi_info():
+    """Ruta para obtener la información completa de la red WiFi."""
+    return jsonify(obtener_info_red())
 
 
 @main.route("/numero_fallas", methods=["GET"])
